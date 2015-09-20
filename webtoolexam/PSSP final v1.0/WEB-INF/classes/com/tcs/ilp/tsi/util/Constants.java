043bd45a2ff36f6000a2cb10c270671835411fa5
Krithikasridharan <sridharan.k@husky.neu.edu>
2015-09-20 00:05:03 -0400

    Gone
    Gone

--- webtoolexam/PSSP final v1.0/WEB-INF/classes/com/tcs/ilp/tsi/util/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.tcs.ilp.tsi.util;
-
-public class Constants {
-	
-	public static final String DRIVER = "oracle.jdbc.driver.OracleDriver";
-	public static final String CONNECTIONURL = "jdbc:oracle:thin:@172.24.137.30:1521:ora10g";
-	public static final String DBUSERNAME = "e772540";
-	public static final String DBPASSWORD = "xddHhtPls";
-}
--- webtoolexam/PSSP final v1.0/jsp/footer.jsp
+++
@@ -1,9 +0,0 @@
-			</div>
-			</tr>
-	  </table>
-	</div>
-	<div id="footercontainer"><center>Copyright © www.mobileworld.com</center></div>
-</div>
-</center>
-</body>
-</html>
